4a5b58e5f0ad761c375c6c74bca5587752f98f50
Lea <lea@floofball.dev>
2024-04-22 16:42:04 +0200

    i think its working now

--- generate.mjs
+++ generate.mjs
@@ -10,28 +10,42 @@ exec("cp -r skel/ repo/");
 exec("git init", { cwd: path.join(".", "repo") });
 exec("git add .", { cwd: path.join(".", "repo") });
 
-for (let x = 0; x < 51; x++) {
-    for (let y = 0; y < 7; y++) {
-        const pos = 7*x + y;
-        const date = dayjs("2017-01-01 GMT+0").add(pos, "day");
+let date = dayjs("2017-01-01 GMT+0");
 
-        if (date.isSame(dayjs('2017-02-02 GMT+0'))) continue; // There already is a contribution listed on March 2nd
+const WIDTH = 52, HEIGHT = 7;
 
+for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
         const col = input.getPixelColor(x, y);
-        if (col == 0x000000ff) {
+        process.stdout.write(col < 0x88888888 ? "x " : "  ");
+    }
+    process.stdout.write("\n");
+}
+
+for (let x = 0; x < WIDTH; x++) {
+    for (let y = 0; y < HEIGHT; y++) {
+        const col = input.getPixelColor(x, y);
+        if (col < 0x88888888 && !date.isSame(dayjs('2017-03-02 GMT+0'))) {
             exec(
                 `git commit -a -m 'ᓚᘏᗢ' --allow-empty`,
                 {
                     cwd: path.join(".", "repo"),
                     env: {
-                        "GIT_AUTHOR_DATE": date.toISOString(),
-                        "GIT_COMMITTER_DATE": date.toISOString(),
+                        // Add 6 hours in case GitHub doesn't like commits being made exactly at midnight
+                        "GIT_AUTHOR_DATE": date.add(6, "hour").toISOString(),
+                        "GIT_COMMITTER_DATE": date.add(6, "hour").toISOString(),
                         ...process.env
                     }
                 }
             );
 
-            console.log(`Created commit for ${date}`);
-        }
+            console.log(`Created commit for ${date} (0x${col.toString(16)})`);
+        } else console.log(`Skipping ${date} (0x${col.toString(16)})`);
+
+        date = date.add(1, "day");
     }
 }
+
+exec("git remote add origin https://github.com/sussycatgirl/trans-rights", { cwd: path.join(".", "repo") });
+exec("git push origin master --force", { cwd: path.join(".", "repo") });
+
